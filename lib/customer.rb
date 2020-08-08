49816133f0c9cc016531f6d8c9f1702e0442572a
Chenyun Zhang <morningchenyun@gmail.com>
2020-08-07 20:48:14 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,38 @@
+require 'pry'
+require_relative 'meal.rb'
+
+
 class Customer
-  
+
+    attr_accessor :name, :age
+
+    @@all = []
+
+    def initialize(name,age)
+        @name = name
+        @age= age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter,total,tip)
+        Meal.new(waiter,self,total,tip)
+    end
+
+    def meals
+        Meal.all.select{|meal| meal.customer == self}
+    end
+
+    def waiters
+        meals.map{|meal| meal.waiter}
+    end
+
 end
+
+# JJ = Customer.new("JJ", 22)
+# Kiki = Customer.new("Kiki", 21)
+# JJ.new_meal("waiter1", 20, 2)
+# puts JJ.meals
